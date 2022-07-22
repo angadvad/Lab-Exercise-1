d5bcde4b39c6f5d7b31806bbe8cc966e40095ba5
Angad <angad@hotmail.co.nz>
2022-07-22 22:53:16 +1200

    DICE-ROLLING #3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//create class "dice" with member function roll
+
 let dice = {
     roll: function (sides) {
       let randomNumber = Math.floor(Math.random() * sides) + 1;
@@ -5,18 +7,20 @@ let dice = {
     }
   }
   
-  //Prints dice roll to the page
+  //Prints dice roll to element e.g. paragraph element with id = "d4"
   
   function printNumber(number,dice) {
     var dice = document.getElementById(dice);
     dice.innerHTML = number;
   }
   
-  var button = document.getElementById('button');
+  //declare variables & arrays
+  let button = document.getElementById('button');
   let diceStr = ['d4','d6','d8','d10','d12','d20']
   let diceNum = [4,6,8,10,12,20];
   let result;
 
+// on button click execute dice rolling function for each type of dice in dice arrays
   button.onclick = function() {
     for (let i=0; i<diceStr.length; i++){
       result = dice.roll(diceNum[i]);
